3d3b45387c27bac260b13fdb1f5f32a04290791c
Nathan Pabst <nathan.pabst@gmail.com>
2018-03-26 21:17:22 -0500

    completed card 1

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-console.log('hello');
+// console.log('hello');
+
+const printToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML = domString;
+};
+
+const domString = (animalArray) => {
+    console.log("animalArray", animalArray);
+    let domString = "";
+    for (let i = 0; i < animalArray.length; i++){
+        domString += `<h1>${animalArray[i].name}</h1>`;
+    }
+    printToDom(domString, 'zoo');
+};
+
+function executeThisFunctionAfterFileLoads (){
+    console.log("executeThisFunctionAfterFileLoads", Date.now());
+    console.log("this", this);
+    console.log("this.responseText", this.responseText);
+    const data = JSON.parse(this.responseText);
+    console.log("data", data);
+    domString(data.animals);
+}
+
+function WTF(){
+    console.log("something went wrong");
+}
+
+const startApplication = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener("load", executeThisFunctionAfterFileLoads);
+    myRequest.addEventListener("error", WTF);
+    myRequest.open("GET", "animals.json");
+    myRequest.send();
+    console.log("myrequest", myRequest);
+};
+startApplication();
